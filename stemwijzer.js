3bbbed125c359e1305363cd9445cee7b137d0c53
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-02-14 10:59:10 +0100

    design aanpassingen + script
    het resultaat wordt nu weergeven

--- stemwijzer.js
+++ stemwijzer.js
@@ -6,7 +6,7 @@ var buttonSkip = document.getElementById("btnSkip");
 var buttonBack = document.getElementById("btnBack");
 
 var countStatement = 0;
-var answer = [];
+var answers = [];
 
 //Onload function
 start();
@@ -26,13 +26,11 @@ function start(){
 //Function agree button
 function buttonEensClicked(){  
   buttonEens.style.backgroundColor = "rgb(0, 174, 255)";
-  buttonOneens.style.backgroundColor = "maroon";
-  buttonNone.style.backgroundColor = "maroon";
 
-  for(let i = 0; i <= subjects[countStatement].parties.length -1; i++){
+  for(let i = 0; i <= subjects[countStatement].parties.length-1; i++){
     if(subjects[countStatement].parties[i].position == "pro"){
       var eens = subjects[countStatement].parties[i].name;
-      answer.push(eens);
+      answers.push(eens);
     }
   }
 
@@ -46,14 +44,11 @@ function buttonEensClicked(){
 //Function disagree button
 function buttonOneensClicked(){
   buttonOneens.style.backgroundColor = "rgb(0, 174, 255)"
-  buttonNone.style.backgroundColor = "maroon";
-  buttonEens.style.backgroundColor = "maroon";
 
-  for(let i = 0; i <= subjects[countStatement].parties.length -1; i++){
+  for(let i = 0; i <= subjects[countStatement].parties.length-1; i++){
     if(subjects[countStatement].parties[i].position == "contra"){
-      console.log(subjects[countStatement].parties[i].name);
       var oneens = subjects[countStatement].parties[i].name;
-      answer.push(oneens);
+      answers.push(oneens);
     }
   }
 
@@ -67,8 +62,6 @@ function buttonOneensClicked(){
 //Function none button
 function buttonNoneClicked(){
   buttonNone.style.backgroundColor = "rgb(0, 174, 255)";
-  buttonEens.style.backgroundColor = "maroon";
-  buttonOneens.style.backgroundColor = "maroon";
 
   countStatement++;
   countResult(countStatement);
@@ -78,28 +71,48 @@ function buttonNoneClicked(){
 
 //Function skip button
 function buttonSkipClicked(){
-  title.innerHTML = subjects[countStatement+1].title;
-  statement.innerHTML = subjects[countStatement+1].statement;
+  title.innerHTML = subjects[countStatement++].title;
+  statement.innerHTML = subjects[countStatement++].statement;
 }
 
 //Function back button
 function buttonBackClicked(){
-  title.innerHTML = subjects[countStatement-1].title;
-  statement.innerHTML = subjects[countStatement-1].statement;
+  title.innerHTML = subjects[countStatement--].title;
+  statement.innerHTML = subjects[countStatement--].statement;
 }
 
 //Function result
 function countResult(countStatement){
   if(countStatement >= subjects.length){
-    result = answer;
-    resultList = answer;
-    Voltooien(result, resultList);
+    const result = answers.reduce((acc, curr) => (acc[curr] = (acc[curr] || 0) + 1, acc), {});
+
+    const sortable = Object.fromEntries(
+      Object.entries(result).sort(([,a],[,b]) => b-a)
+    );
+
+    var sort = sortable[Object.keys(sortable)[0]];
+    var partij = [parties[sort].name,
+                  /*parties[sort].long*/];
+
+    const keys = Object.keys(sortable);
+    delete keys[0]; 
+    console.log(keys);
+
+    const endAnswer = partij;
+    const endAnswer2 = keys;
+
+    Voltooien(endAnswer, endAnswer2);
   }
 }
 
 //Function finished
-function Voltooien(result, resultList){
+function Voltooien(endAnswer, endAnswer2){
   title.innerHTML = ("Resultaat"); 
-  statement.innerHTML = ("De partij die het best bij uw voorkeur past is: " + result); 
-  party.innerHTML = ("Overige partijen: " + resultList); 
+  statement.innerHTML = ("De partij die het best bij uw voorkeur past is: " + endAnswer); 
+  party.innerHTML = ("Overige partijen: " + endAnswer2); 
 }
+
+// 1. overige partijen ophalen (behalve endAnswer)
+// 2. instellen alleen grote / kleine / seculiere partijen tonen
+// 3. extra gewicht geven aan gekozen categorie
+// 4. styling terugzetten bij gegeven antwoord
